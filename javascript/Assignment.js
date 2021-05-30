eaa9ef806b3aa8c06538ff621fe100cf84e5f562
chldydgus777 <chldydgus777@naver.com>
2021-05-30 22:38:42 +0900

    copy 구현

--- javascript/Assignment.js
+++ javascript/Assignment.js
@@ -1,45 +1,41 @@
-var canvas = document.getElementById("Canvas");
-var ctx = canvas.getContext("2d");
+// canvas 요소 찾기
+let canvas = document.getElementById("Canvas");
+// canvas에서 컨텍스트 얻기
+let ctx = canvas.getContext("2d");
 
 // x축, 값이 커질수록 오른쪽으로 이동 
-var Ball_x = 0;
+let Ball_x = 0;
 // y축, 값이 커질수록 아래로 이동
-var Ball_y = 0;
+let Ball_y = 0;
 // x축으로 기울며 속도 빨라짐
-var Ballx_speed = 3;
+let Ballx_speed = 3;
 // y축으로 기울며 속도 빨라짐
-var Bally_speed = 3;
-// 일시정지 재시작 위치
-var position
+let Bally_speed = 3;
+
 
 // 시작 
-const start_button = document.getElementById("start_btn");
+let start_button = document.getElementById("start_btn");
 start_button.addEventListener("click", progress,)
+// 정지
+// const stop_button = document.getElementById("stop_btn");
+// stop_button.addEventListener("click", function(){
+//     stop_button.removeEventListener("click", progress)
+// })
 
 
 function progress() {
-    const start_button = document.getElementById("start_btn");
-    // start_button.disabled = true;
-    if (start_button.innerHTML == "시작"){
-        start_button.innerHTML = "다시 시작"
-    } else { 
-        start_button.innerHTML = "다시 시작"
 
-    }
+    const start_button = document.getElementById("start_btn");
+        start_button.disabled = true;
 
     // 일시정지
-    const pause_button = document.getElementById("pause_btn");
+    let pause_button = document.getElementById("pause_btn");
     pause_button.addEventListener("click",stopinterval)
 
 
-
-    
-    // 복제
-    const copy_button = document.getElementById("copy_btn");
-    // copy_button.addEventListener("click", make_Ball);
     
     // 공 드로잉
-    function drawBall() {
+    const drawBall = () => {
         // 새로운 경로 생성
         ctx.beginPath();
         // 사각형을 그림 (x축, y축, width, height)
@@ -50,21 +46,14 @@ function progress() {
         ctx.fill();
         // 시작 부분과 연결된 직선 추가
         ctx.closePath();
-        // 공 위치에서 Ball_y값이 0보다 작은경우 Bally_speed를 음수로 변경 [상단 튕김]
-        if(Ball_y + Bally_speed < 0) {
+        // 공 위치에서 Ball_y값이 0보다 작은경우, Ball_y값이 Canvas 높이보다 클 때 Bally_speed를 음수로 변경 [상단 튕김]
+        // -15 는 공이 경계를 넘어감을 방지 , OR 연산자 ||
+        if(Ball_y + Bally_speed < 0 || Ball_y + Bally_speed > canvas.height-15) {
             Bally_speed = -Bally_speed;
         }
-        // 공 위치에서 Ball_y값이 Canvas 높이보다 클 때 Bally_speed값을 음수로 변경 [하단 튕김]
+        // 공 위치에서 Ball_x값이 0보다 작은경우, Canvas 높이보다 클 때 Ballx_speed를 음수로 변경 [좌측 튕김]
         // -15 는 공이 경계를 넘어감을 방지
-        if(Ball_y + Bally_speed > canvas.height-15) {
-            Bally_speed = -Bally_speed;
-        }
-        // 공 위치에서 Ball_x값이 0보다 작은경우 Ballx_speed를 음수로 변경 [좌측 튕김]
-        if(Ball_x + Ballx_speed < 0) {
-            Ballx_speed = -Ballx_speed;
-        }
-        // 공 위치에서 Ball_x값이 Canvas 높이보다 클 때 Ballx_speed를 음수로 변경 [우측 튕김]
-        if(Ball_x + Ballx_speed > canvas.width-15) {
+        if(Ball_x + Ballx_speed < 0 || Ball_x + Ballx_speed > canvas.width-15) {
             Ballx_speed = -Ballx_speed;
         }
     }
@@ -79,6 +68,7 @@ function progress() {
         Ball_y += Bally_speed;  
     }
 
+
     // 호출 중지 [일시정지]
     function stopinterval() {
         clearInterval(refreshIntervalId)
@@ -87,30 +77,68 @@ function progress() {
     // 10ms 무한 호출
     const refreshIntervalId = setInterval(draw, 10);
 
-    // function make_Ball() {
-    //     ctx.beginPath();
-    //     ctx.rect(Ball_x-2, Ball_y - 50, 15, 15);
-    //     ctx.fillStyle = "gold";
-    //     ctx.fill();
-    //     ctx.closePath();
+    // 복제
+    const copy_button = document.getElementById("copy_btn");
+    copy_button.addEventListener("click",make_Ball);
+    copy_button.removeEventListener("click",drawBall);
 
-    //     ctx.beginPath();
-    //     ctx.rect(Ball_x + 50, Ball_y-2, 15, 15);
-    //     ctx.fillStyle = "red";
-    //     ctx.fill();
-    //     ctx.closePath();
 
-    //     ctx.beginPath();
-    //     ctx.rect(Ball_x + 50, Ball_y-50, 15, 15);
-    //     ctx.fillStyle = "blue";
-    //     ctx.fill();
-    //     ctx.closePath();
-
-    // }
 
 }
-    // 정지
-    const stop_button = document.getElementById("stop_btn");
-    stop_button.addEventListener("click", function(){
-        stop_button.removeEventListener("click", progress)
-    })
+function make_Ball() {
+    // 시작
+    const start_button = document.getElementById("start_btn");
+    start_button.disabled = true;
+
+    // 일시정지
+    let pause_button = document.getElementById("pause_btn");
+    pause_button.addEventListener("click",new_stopinterval)
+
+        const new_drawBall = () => {
+        ctx.beginPath();
+        ctx.rect(Ball_x-20, Ball_y-20, 15, 15);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.rect(Ball_x-20, Ball_y+20, 15, 15);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.rect(Ball_x+20, Ball_y+20, 15, 15);
+        ctx.fillStyle = "green";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.rect(Ball_x+20, Ball_y-20, 15, 15);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+        ctx.closePath();
+        if(Ball_y + Bally_speed < 0 || Ball_y + Bally_speed > canvas.height-35) {
+            Bally_speed = -Bally_speed;
+        }
+        if(Ball_x + Ballx_speed < 0 || Ball_x + Ballx_speed > canvas.width-35) {
+            Ballx_speed = -Ballx_speed;
+        }}
+    
+            // 복제 드로잉
+            function new_draw() {
+                // 흔적 (이전프레임)Rect 부분을 지우는 코드
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                new_drawBall();
+                // Ball_x, Ball_y 축에 Ballx_speed,Bally_speed를 더해줘서 Ball_x, Ball_y 갱신
+                Ball_x += Ballx_speed;
+                Ball_y += Bally_speed;
+            }
+        // 호출 중지 [일시정지]
+        function new_stopinterval() {
+        clearInterval(new_refreshInterval)
+    }
+        // 10ms 무한 호출   
+        const new_refreshInterval = setInterval(new_draw, 10);
+    }
+
